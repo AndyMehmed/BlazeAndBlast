f7acc4157f12894e3453d8eaef958033bfa352f0
Haamps <hampe097@gmail.com>
2023-04-18 15:19:30 +0200

    Player.js updated, folder "img" created, "javascript.js" removed
    Updated the player.js file with a draw function that draws out the palyer on the canvas. Also removed "javascript.js" as it had no function. And added a foled named "img" where the group can add player images and background images to the game.

--- js/player.js
+++ js/player.js
@@ -17,4 +17,21 @@ class Player {
         }
         this.gravity = 1
     }
+
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+        this.sides.bottom = this.position.y + this.height
+
+        // above bottom of canvas
+        if (this.sides.bottom + this.velocity.y < canvas.height) {
+            this.velocity.y += this.gravity
+        } else this.velocity.y = 0
+    }
 }
