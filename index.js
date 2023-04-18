6bd8113be38bac9ae9662a563d2ba983e28f3b4a
JeNordahl <jesper.nordahl@hotmail.com>
2023-04-18 14:54:08 +0200

    Created index.html, updated CSS and JS
    Created index.html, updated index.js and updated style.css

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ const keys = {
         pressed: false,
     },
 }
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+
+    player.velocity.x = 0
+    if (keys.d.pressed) player.velocity.x = 5
+    else if (keys.a.pressed) player.velocity.x = -5
+    player.draw()
+    player.update()
+}
