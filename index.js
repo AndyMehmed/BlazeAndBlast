a9792fefedc7cca4c8339a2e073cf0ac8b66a1c8
DavidRadic <david.radic@icloud.com>
2023-05-12 14:04:36 +0200

    Added Level 4
    I have added map Level 4 design, code that includes the collisionblocks, added it to the index javascript file.

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ const player = new Player({
                         level++
 
                         //REMOVE BEFORE ADDING MORE LEVELS
-                        if (level ===4) level = 1
+                        if (level ===5) level = 1
                         levels[level].init()
                         player.switchSprite('idleRight')
                         player.preventInput = false
@@ -223,6 +223,42 @@ let levels = {
         },
     },
 
+
+    //-----LEVEL 4-----//
+    4: {
+        init: () => {
+            parsedCollisions = collisionsLevel4.parse2D()
+            collisionBlocks = parsedCollisions.createObjectsFrom2D()
+            player.collisionBlocks = collisionBlocks
+            player.position.x = 750
+            player.position.y = 230
+
+            if(player.currentAnimation) player.currentAnimation.isActive = false
+
+            background = new Sprite({
+                position: {
+                x: 0,
+                y: 0,
+            },
+            imageSrc: './img/Level4.png',
+            })
+
+            doors = [
+                new Sprite({
+                    position: {
+                        x: 175,
+                        y: 336,
+                    },
+                    imageSrc: './img/doorOpen.png',
+                    frameRate: 5,
+                    frameBuffer: 5,
+                    loop: false,
+                    autoplay: false,
+                }),
+            ]
+        },
+    },
+    
 }
 
 // Funktion som gör att spelet tror att man inte trycker på tangenterna, 
