391ff4adb6932042471c1b101396f3f735bd545b
Haamps <hampe097@gmail.com>
2023-05-15 15:35:10 +0200

    Updated enemies

--- js/classes/Enemies.js
+++ js/classes/Enemies.js
@@ -20,67 +20,66 @@ class Enemy {
         c.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 
-update(player) {
-    const dx = player.position.x - this.position.x;
-    const dy = player.position.y - this.position.y;
-    const distance = Math.sqrt(dx * dx + dy * dy);
-
-    // Only move towards the player if the distance is less than or equal to 200
-    if (distance <= 200) {
-        this.followingPlayer = true;
-    }
-
-    if (this.followingPlayer) {
-        this.velocity.x = (dx / distance) * this.speed;
-        this.velocity.y = (dy / distance) * this.speed;
-        // Check for collisions with the collision blocks
-        for (let i = 0; i < collisionBlocks.length; i++) {
-            const collisionBlock = collisionBlocks[i];
-
-            // If a collision exists
-            if (
-                this.position.x <= collisionBlock.position.x + collisionBlock.width &&
-                this.position.x + this.width >= collisionBlock.position.x &&
-                this.position.y + this.height >= collisionBlock.position.y &&
-                this.position.y <= collisionBlock.position.y + collisionBlock.height
-            ) {
-                // Collision on x-axis going to the left
-                if (this.velocity.x < 0) {
-                    const offset = this.position.x - collisionBlock.position.x;
-                    this.position.x = collisionBlock.position.x + collisionBlock.width + offset + 0.01;
-                    this.velocity.x = 0;  // Reset velocity
-                    break;
+    update(player) {
+        const dx = player.position.x - this.position.x;
+        const dy = player.position.y - this.position.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+    
+        if (distance <= 200) {
+            this.followingPlayer = true;
+        }
+    
+        if (this.followingPlayer) {
+            let newVelocityX = (dx / distance) * this.speed;
+            let newVelocityY = (dy / distance) * this.speed;
+    
+            // First, handle movement and collision in the x-direction
+            this.velocity.x = newVelocityX;
+            this.position.x += this.velocity.x;
+            for (let i = 0; i < collisionBlocks.length; i++) {
+                const collisionBlock = collisionBlocks[i];
+                if (
+                    this.position.x < collisionBlock.position.x + collisionBlock.width &&
+                    this.position.x + this.width > collisionBlock.position.x &&
+                    this.position.y + this.height > collisionBlock.position.y &&
+                    this.position.y < collisionBlock.position.y + collisionBlock.height
+                ) {
+                    if (this.velocity.x < 0) {
+                        this.position.x = collisionBlock.position.x + collisionBlock.width + 0.01;
+                        this.velocity.x = 0;  // Reset velocity
+                        break;
+                    }
+                    if (this.velocity.x > 0) {
+                        this.position.x = collisionBlock.position.x - this.width - 0.01;
+                        this.velocity.x = 0;  // Reset velocity
+                        break;
+                    }
                 }
-
-                // Collision on x-axis going to the right
-                if (this.velocity.x > 0) {
-                    const offset = this.position.x + this.width - collisionBlock.position.x;
-                    this.position.x = collisionBlock.position.x - this.width - offset - 0.01;
-                    this.velocity.x = 0;  // Reset velocity
-                    break;
-                }
-
-                // Collision on y-axis going upwards
-                if (this.velocity.y < 0) {
-                    const offset = this.position.y - collisionBlock.position.y;
-                    this.position.y = collisionBlock.position.y + collisionBlock.height + offset + 0.01;
-                    this.velocity.y = 0;  // Reset velocity
-                    break;
-                }
-
-                // Collision on y-axis going downwards
-                if (this.velocity.y > 0) {
-                    const offset = this.position.y + this.height - collisionBlock.position.y;
-                    this.position.y = collisionBlock.position.y - this.height - offset - 0.01;
-                    this.velocity.y = 0;  // Reset velocity
-                    break;
+            }
+    
+            // Then, handle movement and collision in the y-direction
+            this.velocity.y = newVelocityY;
+            this.position.y += this.velocity.y;
+            for (let i = 0; i < collisionBlocks.length; i++) {
+                const collisionBlock = collisionBlocks[i];
+                if (
+                    this.position.x < collisionBlock.position.x + collisionBlock.width &&
+                    this.position.x + this.width > collisionBlock.position.x &&
+                    this.position.y + this.height > collisionBlock.position.y &&
+                    this.position.y < collisionBlock.position.y + collisionBlock.height
+                ) {
+                    if (this.velocity.y < 0) {
+                        this.position.y = collisionBlock.position.y + collisionBlock.height + 0.01;
+                        this.velocity.y = 0;  // Reset velocity
+                        break;
+                    }
+                    if (this.velocity.y > 0) {
+                        this.position.y = collisionBlock.position.y - this.height - 0.01;
+                        this.velocity.y = 0;  // Reset velocity
+                        break;
+                    }
                 }
             }
         }
-
-        // Move the enemy
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
     }
-}
 }
