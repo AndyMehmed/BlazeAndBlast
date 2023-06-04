8ca8bad17d3e11fd6a8afa5c4a15e7555247e61a
DavidRadic <david.radic@icloud.com>
2023-06-04 03:04:35 +0200

    Revert "Merge branch 'MainCopy'"
    This reverts commit ad552fd6559e04611ba498e450f7a17f10f0e18b, reversing
    changes made to 4317d52607fee2272f275f1289f89270391ae413.

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,3 @@
-//Canvas with is 70 tiles so the program is looking through the 70 tiles and then "jump down" a row to check the next 70 tiles
 Array.prototype.parse2D = function() {
     const rows =  []
     for (let i = 0; i < this.length; i+=70) {
@@ -7,9 +6,7 @@ Array.prototype.parse2D = function() {
 
     return rows
 }
-//After the program has looked through all of the rows it looks for symbol 578 and 250.
-//Symbol 578 is where the collisionblocks are spawned and 250 is where our door is supposed to be interacted with.
-//It then draws out these 2 blocks on our canvas based on the code given in "collisions.js"
+
 Array.prototype.createObjectsFrom2D = function () {
     const objects = []
     this.forEach((row, y) => {
